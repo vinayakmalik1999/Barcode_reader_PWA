3fe46546d122b610398e3783e0fc98f526535aa3
vmalik <vinayakmalik1999@gmail.com>
2019-06-28 11:20:42 -0500

    changes

--- barcode-reader/src/App.js
+++ barcode-reader/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
-
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <Router>
+          <div>
+
+            <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            </ul>
+            <div className="content">
+            <Switch>
+             <Route exact path="/" component={Home}/>
+             <Route path="/stuff" component={Stuff}/>
+             <Route path="/contact" component={Contact}/>
+             </Switch>
+              </div>
+          </div>
+        </Router>
   );
 }
 
