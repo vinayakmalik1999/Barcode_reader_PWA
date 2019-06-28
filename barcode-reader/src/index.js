b5f3da6df25626c3aad2ae095bf603441cc56eef
vmalik <vinayakmalik1999@gmail.com>
2019-06-28 17:06:31 -0500

    adding bootstrap

--- barcode-reader/src/index.js
+++ barcode-reader/src/index.js
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
