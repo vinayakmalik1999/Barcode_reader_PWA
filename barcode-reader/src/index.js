4adb30092195a68d04a5769e50d0833c6f09be07
vmalik <vinayakmalik1999@gmail.com>
2019-07-09 11:32:58 -0500

    more changes

--- barcode-reader/src/index.js
+++ barcode-reader/src/index.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Bootstrap from 'react-bootstrap';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
