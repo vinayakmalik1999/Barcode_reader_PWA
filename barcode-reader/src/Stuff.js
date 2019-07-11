5770c2cf80d8adab617806ae6e39fe36d0897c64
vmalik <vinayakmalik1999@gmail.com>
2019-07-11 10:51:29 -0500

    major updates to table refresh

--- barcode-reader/src/Stuff.js
+++ barcode-reader/src/Stuff.js
@@ -5,6 +5,8 @@ import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
 import OfflineBanner from './OfflineBanner.js'
 import InputGroup from 'react-bootstrap/InputGroup'
+import OfflineTable from './OfflineTable.js'
+
 class UserPage extends Component {
   // Setting initial state
 
@@ -13,6 +15,8 @@ class UserPage extends Component {
       this.state = {
         user:[],
         formValue:'',
+        offlineUser:[],
+        resValue:'',
         offlineSubmitValue:''
       }
       this.handleChange = this.handleChange.bind(this);
@@ -47,34 +51,110 @@ return;
   {
     Code:this.state.formValue
   })
+
      .then(res => {
        console.log(res);
-       console.log(res.data);
+       this.setState({resValue:res.data});
+       axios.get('https://kt-dev.outsystemscloud.com/PWABack/rest/BarCode/GetList')
+       .then(response => {
+
+         this.setState({user:response.data});
+       })
+       .catch(error => {
+         return caches.match('https://kt-dev.outsystemscloud.com/PWABack/rest/BarCode/GetList')
+         .then(response =>{
+           this.setState({user:response.data});
+           
+         })
+
+       });
        this.setState({formValue:''})
-       window.location.reload();
+
      })
      .catch(error => {
        navigator.serviceWorker.controller.postMessage(this.state.formValue)
        console.log("Offline sending data to SW");
        this.setState({formValue:''})
-       
      })
 
 
 }
 handleChange(event){
   this.setState({formValue:event.target.value})
+  this.setState({offlineSubmitValue:this.state.formValue})
+
+
 }
 
 
   // Return a table with some data from the API.
+
   render(){
 
+if(!navigator.onLine){
 
+return(
+  <div className="container">
+<OfflineBanner/>
+
+          <form onSubmit ={this.handleSubmit}>
+            <input type ="string" value = {this.state.formValue} onChange={this.handleChange}/>
+            <Button type="submit" variant="light">Add</Button>
+            <Link to ="/contact">
+          <Button variant="light">Scan</Button>
+          </Link>
+            </form>
+
+            <Table striped bordered hover variant="dark">
+                  <thead>
+                  <tr><th>OFFLINE RESULTS</th></tr>
+                    <tr>
+                      <th>ID</th>
+                      <th>Code</th>
+                      <th>Created Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {this.state.offlineUser.map((users, index)=> (
+                    <tr key ={index}>
+                      <td >{users.Id}</td>
+                      <td >{users.Code}</td>
+                      <td >{users.createdDate}</td>
+                      </tr>
+                  ))}
+
+
+                      </tbody>
+                    </Table>
+
+<Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Code</th>
+          <th>Created Date</th>
+        </tr>
+      </thead>
+      <tbody>
+
+  {this.state.user.map((users, index)=> (
+    <tr key ={index}>
+      <td >{users.Id}</td>
+      <td >{users.Code}</td>
+      <td >{users.createdDate}</td>
+      </tr>
+  ))}
+
+
+      </tbody>
+    </Table>
+  </div>
+
+)
+}
   return  (
 
     <div className="container">
-<OfflineBanner/>
 
             <form onSubmit ={this.handleSubmit}>
               <input type ="string" value = {this.state.formValue} onChange={this.handleChange}/>
